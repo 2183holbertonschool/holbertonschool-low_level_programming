ae27b0e3c1990e2345579106b8d3cb7bcf1d83bb
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-03 21:39:44 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of int data type: %lu\n", sizeof(int));
+	printf("Size of int data type : %lu\n",sizeof(int));
+	printf("Size of char data type : %lu\n",sizeof(char));
+	printf("Size of float data type : %lu\n",sizeof(float));
+	printf("Size of double data type : %lu\n",sizeof(double));
 	return (0);
 }
