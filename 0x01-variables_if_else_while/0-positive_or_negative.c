c1632da37007606daeb42f2c1f372ee7e62fae0b
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-04 17:11:53 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	};
 	if (n == 0)
 	{
-		printf("%i is zero\n",n);
+		printf("%i is zero\n", n);
 	};
 	if (n < 0)
 	{
-		printf("%i is negative\n",n);
+		printf("%i is negative\n", n);
 	};
 	return (0);
 }
