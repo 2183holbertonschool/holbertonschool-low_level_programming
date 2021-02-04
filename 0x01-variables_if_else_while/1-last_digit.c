45713e38abec509a39a37d58242b08e2f83b9cdd
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-04 19:44:41 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	r = n % 10;
 	if (r > 5)
 	{
-		printf("Last digit of %i is %i and greater than 5\n", n, r);
+		printf("Last digit of %i is %i and greater is than 5\n", n, r);
 	}
 	if (r == 0)
 	{
