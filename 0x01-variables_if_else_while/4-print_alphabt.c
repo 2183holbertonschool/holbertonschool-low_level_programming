8a01896410339c651d3d99cbc5687c15ff5ba079
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-04 22:28:58 +0000

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,21 +7,11 @@
  **/
 int main(void)
 {
-<<<<<<< HEAD
-  char ch;
+	char ch;
 
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    if (ch != 'e' && ch != 'q')
-      putchar(ch);
-  putchar('\n');
-  return (0);
-=======
-        char ch;
-
-        for (ch = 'a' ; ch <= 'z' ; ch++)
-                if (ch != 'e' && ch != 'q')
-                        putchar(ch);
-        putchar('\n');
-        return (0);
->>>>>>> 2e9b0b9d65d392dc5dcb44766db266ada027ad46
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	putchar('\n');
+	return (0);
 }
