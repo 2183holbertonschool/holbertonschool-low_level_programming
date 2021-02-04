d2272b63de82045ce1882ba7ea06a93648ae538a
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-04 22:37:53 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ **/
+int main(void)
+{
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
