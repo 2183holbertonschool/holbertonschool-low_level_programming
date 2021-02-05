8fff959ce2cfd501ad26fa5a71d8bb5338c99168
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-05 01:17:11 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ **/
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48 ; i <= 57 ; i++)
+		for (j = 48 ; j <= 57 ; j++)
+			for (k = 48 ; k <= 57 ; k++)
+				for (l = 49 ; l <= 57 ; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+				if (i != 57 || j != 57 || k != 57 || l != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}
