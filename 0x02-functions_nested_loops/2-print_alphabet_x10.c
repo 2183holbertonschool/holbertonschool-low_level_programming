2f60a7e3936ad594193e17a68df13dbe22c01996
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-08 20:40:48 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+void print_alphabet_x10(void)
+{
+        char ch;
+	int i;
+
+	for (i = 0 ; i<= 9 ; i++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
