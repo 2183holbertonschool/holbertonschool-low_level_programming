8c13cecce45cd7292c0771ec4e44be277d1fe781
Eduardo Herrera Forero <2183@holbertonschool.com>
2021-02-09 15:46:42 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result
+ * @i: first number
+ * @j: second number
+ *
+ * Description: adds i plus j
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
